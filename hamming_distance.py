6f62ebb56b3041b5218269b3e54ca9ef44e3e95c
Abhinav Manchanda <abhinav@abhinav.local>
2022-05-11 17:00:58 +0530

    Break line into 2 as it was exceeding the length

--- hamming_distance.py
+++ hamming_distance.py
@@ -6,7 +6,8 @@ def ApproximatePatternCount(Pattern, Text, d):
     return pattern_count_with_mismatch(Text, Pattern, d)
 
 def pattern_count_with_mismatch(text, pattern, mismatch_threshold):
-    return sum(1 for i in range(len(text) - len(pattern) + 1) if HammingDistance(text[i:i + len(pattern)], pattern) <= mismatch_threshold)
+    return sum(1 for i in range(len(text) - len(pattern) + 1)
+               if HammingDistance(text[i:i + len(pattern)], pattern) <= mismatch_threshold)
 
 def ApproximatePatternMatching(Text, Pattern, d):
     return list(i for i in range(len(Text) - len(Pattern) + 1) if HammingDistance(Text[i:i + len(Pattern)], Pattern) <= d)
