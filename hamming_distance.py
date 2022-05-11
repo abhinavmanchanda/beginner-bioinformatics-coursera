1a76a81740588cf78813393f094a3eb20afe81b2
Abhinav Manchanda <abhinav@abhinav.local>
2022-05-11 16:54:07 +0530

    Modify approx pattern count and approx pattern match to use functional programming, add tests

--- hamming_distance.py
+++ hamming_distance.py
@@ -3,19 +3,13 @@ from functools import reduce
 # Input:  Strings Pattern and Text, and an integer d
 # Output: The number of times Pattern appears in Text with at most d mismatches
 def ApproximatePatternCount(Pattern, Text, d):
-    return len(ApproximatePatternMatching(Text, Pattern, d))
+    return pattern_count_with_mismatch(Text, Pattern, d)
+
+def pattern_count_with_mismatch(text, pattern, mismatch_threshold):
+    return sum(1 for i in range(len(text) - len(pattern) + 1) if HammingDistance(text[i:i + len(pattern)], pattern) <= mismatch_threshold)
 
 def ApproximatePatternMatching(Text, Pattern, d):
-    positions = []
-    for i in range(0, len(Text)+1-len(Pattern)):
-        hamming_distance = HammingDistance(Text[i:i+len(Pattern)], Pattern)
-        if is_approximate_match(hamming_distance, d):
-            positions = positions + [i]
-    return positions
-
-def is_approximate_match(hamming_distance, threshold):
-    return hamming_distance <= threshold
-
+    return list(i for i in range(len(Text) - len(Pattern) + 1) if HammingDistance(Text[i:i + len(Pattern)], Pattern) <= d)
 
 def HammingDistance(p, q):
     return reduce(lambda a, b: a + b, list(map(lambda x, y: int(x != y), list(p), list(q))), 0)
--- hamming_distance_test.py
+++ hamming_distance_test.py
@@ -1,6 +1,17 @@
 import unittest
 from hamming_distance import HammingDistance
+from hamming_distance import ApproximatePatternCount
+from hamming_distance import ApproximatePatternMatching
 
 class HammingDistanceTest(unittest.TestCase):
-    def test_should_find_hamming_distance_between_two_strings(self):
+    def test_find_hamming_distance_between_two_strings(self):
         self.assertEqual(3, HammingDistance('GGGCCGTTGGT', 'GGACCGTTGAC'))
+
+    def test_find_approximate_pattern_count_with_mismatch_threshold(self):
+        self.assertEqual(4, ApproximatePatternCount('GAGG', 'TTTAGAGCCTTCAGAGG', 2))
+
+    def test_find_indices_of_approximate_pattern_matches_with_given_threshold(self):
+        pattern = 'ATTCTGGA'
+        dna = 'CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAAT'
+        mismatch_threshold = 3
+        self.assertEqual([6, 7, 26, 27], ApproximatePatternMatching(dna, pattern, mismatch_threshold))
